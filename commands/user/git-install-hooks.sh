ba1c66c65797572e1a46648a226eb7011d9b595c
Sergiy Noskov <noskov.org@gmail.com>
2025-02-28 13:55:52 +0100

    Add git-install-hooks.sh

---
+++ commands/user/git-install-hooks.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# ---
+# @description      Installs Git hooks for:
+#                     - Conventional Commits validation
+# @author           Sergiy Noskov <sergiy@noskov.org>
+# @copyright        Ergiosko <contact@ergiosko.com>
+# @license          MIT
+# @version          1.0.0
+# @since            1.0.0
+# @category         User Commands
+# @dependencies     - Bash 4.4.0 or higher
+#                   - Git 2.0 or higher
+# ---
+
+is_sourced || exit 1
+
+declare -gA ROOTINE_SCRIPT_ARGS=(
+  [force]="Force installation (overwrite existing):0:${1:-false}:^(true|false)$"
+)
+
+main() {
+  handle_args "$@"
+
+  if ! git_install_commit_hook "${ROOTINE_SCRIPT_ARG_FORCE}"; then
+    return 1
+  fi
+
+  return 0
+}
+
+main "$@"
